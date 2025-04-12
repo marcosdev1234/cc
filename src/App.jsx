d56882e9509f12851be73f9c366ce994d20dc024
marcosdev1234 <polaskymarcos@gmail.com>
2025-04-12 10:26:24 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,17 @@ import { Routes, Route, Link } from 'react-router-dom';
 import Home from './pages/Home';
 import SimpleRugStats from './pages/SimpleRugStatus';
 import Info from './pages/Info';
+import Menu from './pages/Menu';
+import RugHistory from './pages/RugHistory';
 
   function AppRoutes() {
       return (
           <div>
              
               <Routes>
-           
-                  <Route path="/" element={<Home/>} />
+                 <Route path="/" element={<Menu/>} />
+                 <Route path="/rugHistory" element={<RugHistory/>} />
+                  <Route path="/create" element={<Home/>} />
                   <Route path="/about" element={<SimpleRugStats />} />
                   <Route path="/info" element={<Info />} />
               </Routes>

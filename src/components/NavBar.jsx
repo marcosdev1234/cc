382dcec0cf1edafde94b3540d11e8afe896ab645
marcosdev1234 <polaskymarcos@gmail.com>
2025-04-15 15:10:57 -0600

    Update NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,6 +4,9 @@ import InfoOutlineIcon from '@mui/icons-material/InfoOutline';
 import QueryStatsIcon from '@mui/icons-material/QueryStats';
 import CoinDisplay from './CoinDisplay.jsx';
 import { Link } from 'react-router-dom';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import XIcon from '@mui/icons-material/X';
+import TelegramIcon from '@mui/icons-material/Telegram';
 import "../App.css"
 const NavBar = () => {
   return (
@@ -16,7 +19,16 @@ const NavBar = () => {
       </Link>
       <CoinDisplay />
       </div>
-      <div>
+      <div style={{display:"flex"}}>
+      <Link to={"https://t.me/letmerug"}>
+      <TelegramIcon  sx={{color:"#675BF0",fontSize:"25px"}}/>
+       </Link>
+      <Link to={"https://x.com/RugPullfest"}>
+      <XIcon  sx={{color:"#675BF0",fontSize:"25px"}}/>
+       </Link>
+      <Link to={"https://www.facebook.com/profile.php?id=61575302158529"}>
+      <FacebookIcon sx={{color:"#675BF0",fontSize:"25px"}}/>
+       </Link>
     <Link to={"/info"}>
     <InfoOutlineIcon sx={{color:"#675BF0",fontSize:"25px"}}/>
     </Link>
